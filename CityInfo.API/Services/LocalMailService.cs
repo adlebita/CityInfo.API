d8ce39109b6c1747031460b9588e9bbc06e38305
LebitaA <adrian.lebita@ospri.co.nz>
2022-09-12 07:00:28 +1200

    DI Config into MailService classes, add mailsettings into appsettings.json

--- CityInfo.API/Services/LocalMailService.cs
+++ CityInfo.API/Services/LocalMailService.cs
@@ -2,9 +2,15 @@ namespace CityInfo.API.Services;
 
 public class LocalMailService : IMailService
 {
-    private string _mailTo = "admin@mycompany.com";
-    private string _mailFrom = "noreply@mycompany.com";
-
+    private readonly string _mailTo;
+    private readonly string _mailFrom;
+    
+    public LocalMailService(IConfiguration configuration)
+    {
+        _mailTo = configuration["MailSettings:MailTo"];
+        _mailFrom = configuration["MailSettings:MailFrom"];
+    }
+    
     public void Send(string subject, string message)
     {
       Console.WriteLine($"Mail from {_mailFrom} to {_mailTo} with " + nameof(LocalMailService));  
